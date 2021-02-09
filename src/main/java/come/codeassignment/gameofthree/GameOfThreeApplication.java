f3e2a79fd31fe50a7f08906d91a27cd393906786
aligolgol <golgol.postbox@gmail.com>
2021-02-09 12:21:48 +0330

    adding swagger

--- src/main/java/come/codeassignment/gameofthree/GameOfThreeApplication.java
+++ src/main/java/come/codeassignment/gameofthree/GameOfThreeApplication.java
@@ -4,8 +4,10 @@ import come.codeassignment.gameofthree.config.PropertiesConfig;
 import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class GameOfThreeApplication {
 
     public static void main(String[] args) {
--- src/test/java/come/codeassignment/gameofthree/controller/AcceptanceTest.java
+++ src/test/java/come/codeassignment/gameofthree/controller/AcceptanceTest.java
@@ -3,13 +3,17 @@ package come.codeassignment.gameofthree.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import come.codeassignment.gameofthree.gameRound.domain.InputGameRound;
+import come.codeassignment.gameofthree.service.GameService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -25,7 +29,9 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@TestPropertySource("/application.properties")
+//@TestPropertySource("/application.properties")
+@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+@ExtendWith(SpringExtension.class)
 public class AcceptanceTest {
 
     MockMvc mockMvc;
@@ -36,16 +42,20 @@ public class AcceptanceTest {
     @Autowired
     GameController gameController;
 
+    @Autowired
+    GameService gameService;
+
     @Before
     public void setup() throws Exception {
         this.mockMvc = standaloneSetup(this.gameController).build();// Standalone context
     }
-    @Test
 
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     public void should_determineTheWinner_when_thereIsAPossibleOne() throws Exception {
         String createUri = String.format("/api/v1/create?playerType=%s", 1);
         String playUri = "/api/v1/play";
-        InputGameRound inputGameRound = new InputGameRound(-1,10);
+        InputGameRound inputGameRound = new InputGameRound(-1, 10);
 
         ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String json = writer.writeValueAsString(inputGameRound);
@@ -64,7 +74,7 @@ public class AcceptanceTest {
                 .andExpect(status().isOk())
                 .andDo(print());
 
-        inputGameRound = new InputGameRound(0,3);
+        inputGameRound = new InputGameRound(0, 3);
         json = writer.writeValueAsString(inputGameRound);
         MvcResult result = mockMvc.perform(post(playUri)
                 .accept(MediaType.APPLICATION_JSON)
@@ -74,6 +84,103 @@ public class AcceptanceTest {
                 .andDo(print())
                 .andReturn();
 
-        assertTrue("The winner is PLAYER 2",result.getResponse().getContentAsString().contains("The winner is PLAYER 2"));
+        assertTrue("The winner is PLAYER ", result.getResponse().getContentAsString().contains("The winner is PLAYER "));
     }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void should_returnMessageThatYouNeedToWaitToAnother_when_thereIsNotEnoughPlayersToPlay() throws Exception {
+
+        String createUri = String.format("/api/v1/create?playerType=%s", 1);
+        String playUri = "/api/v1/play";
+        InputGameRound inputGameRound = new InputGameRound(-1, 10);
+
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = writer.writeValueAsString(inputGameRound);
+
+        mockMvc.perform(get(createUri).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("0"));
+
+        MvcResult result = mockMvc.perform(post(playUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        assertTrue("You should wait to another player to start",
+                result.getResponse().getContentAsString().contains("you should wait to another player to start"));
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void should_returnMessageYourNumberIsNotDividable_when_theInputNumberIsNotDividableByThree() throws Exception {
+        String createUri = String.format("/api/v1/create?playerType=%s", 1);
+        String playUri = "/api/v1/play";
+        InputGameRound inputGameRound = new InputGameRound(-1, 10);
+
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = writer.writeValueAsString(inputGameRound);
+
+        mockMvc.perform(get(createUri).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("0"));
+
+        Thread.sleep(3000);
+        mockMvc.perform(get(createUri).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("10"));
+
+        mockMvc.perform(post(playUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        inputGameRound = new InputGameRound(0, 4);
+        json = writer.writeValueAsString(inputGameRound);
+        MvcResult result = mockMvc.perform(post(playUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        assertTrue("The 4 is not dividable by three", result.getResponse().getContentAsString().contains("The 4 is not dividable by three"));
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void should_beAbleToPlay_when_theCompetitorIsMachine() throws Exception {
+        String createUri = String.format("/api/v1/create?playerType=%s", 2);
+        String playUri = "/api/v1/play";
+        InputGameRound inputGameRound = new InputGameRound(-1, 10);
+
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = writer.writeValueAsString(inputGameRound);
+
+        mockMvc.perform(get(createUri).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("10"));
+
+        mockMvc.perform(post(playUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        inputGameRound = new InputGameRound(0, 3);
+        json = writer.writeValueAsString(inputGameRound);
+        MvcResult result = mockMvc.perform(post(playUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        assertTrue("The winner is PLAYER 2", result.getResponse().getContentAsString().contains("The winner is PLAYER 2"));
+    }
+
 }
